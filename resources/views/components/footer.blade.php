07b0b37780704f1746b4da2fd4d0d163585a2100
rrivera <rrivera@redbannermedia.com>
2024-08-31 15:23:14 -0400

    footer and icons set up

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,9 +1,30 @@
 <footer class="flex flex-row w-full mb-3">
     <div class="flex flex-row w-full">
         <div class="bg-white py-1 px-3 mx-2 my-2 text-2xl flex flex-row">
-            R
+            <ul class="flex flex-row justify-between content-center place-items-center">
+                <li>
+                    <a href="https://www.threads.net/@xerafenix" target="_blank" class="group">
+                        @include('components.icons.threads')
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/xerafenix/" target="_blank" class="group">
+                        @include('components.icons.instagram')
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.upwork.com/freelancers/~01dca66f043497b720" target="_blank" class="group">
+                        @include('components.icons.upwork')
+                    </a
+                </li>
+                <li>
+                    <a href="https://github.com/digitalvillainy" target="_blank" class="group">
+                        @include('components.icons.github')
+                    </a
+                </li>
+            </ul>
         </div>
-        <div class="bg-neon-green py-1 px-3 m-2 text-xl text-right w-full">
+        <div class="bg-neon-green py-1 px-3 m-2 text-xl text-right w-full place-content-center">
             Made By Roberto Rivera &copy; 2024
         </div>
     </div>
