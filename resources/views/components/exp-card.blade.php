42e69e7ff5f88cffe3851d1b6c0e311e4cb1d82a
Roberto Rivera <rrivera@redbannermedia.com>
2024-09-08 15:16:11 -0400

    Updates switching device

--- resources/views/components/exp-card.blade.php
+++ resources/views/components/exp-card.blade.php
@@ -1,9 +1,18 @@
-@props(['title', 'company', 'from', 'to', 'description'])
-<div class="border-2 border-dark-neon-green rounded flex flex-col p-3 w-[30em] text-white justify-between text-xl space-y-3">
-    <h3 class="text-3xl">{{ $company }}</h3>
-    <div class="flex flex-row justify-between">
-        <span>{{ $title }}</span>
-        <span>{{ $from }} - {{ $to }}</span>
+@props(['title', 'company', 'from', 'to', 'description', 'position' => 'left'])
+<div class="flex flex-row relative">
+    <div
+        class="border-2 border-dark-neon-green rounded flex flex-col p-3 w-[30em] text-white justify-between text-xl space-y-3">
+        <h3 class="text-3xl">{{ $company }}</h3>
+        <div class="flex flex-row justify-between">
+            <span>{{ $title }}</span>
+            <span>{{ $from }} - {{ $to }}</span>
+        </div>
+        <p class="text-neon-green">{{ $description }}</p>
     </div>
-    <p class="text-neon-green">{{ $description }}</p>
+    <div @class([
+        'bg-white h-1.5 w-28 absolute top-1/2',
+        '-left-28' => $position === 'left',
+         '-right-28' => $position === 'right'
+     ])
+    ></div>
 </div>
