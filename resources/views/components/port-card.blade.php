daa1858df8ffb8501cddf23bab6eeacfeb67b3b4
rrivera <rrivera@redbannermedia.com>
2024-09-12 20:02:52 -0400

    switching devices

--- resources/views/components/port-card.blade.php
+++ resources/views/components/port-card.blade.php
@@ -1,3 +1,42 @@
-<section class="border-2 border-neon-green rounded flex flex-col p-1.5 w-40 h-full my-6">
-    test
+@props(['imgs', 'title', 'description', 'languages', 'ghLink' => false, 'siteLink' => false])
+<section class="border-2 border-neon-green rounded flex flex-col p-1.5 max-w-80 h-full my-6">
+{{--    <div class="bg-[url('{{asset($imgs[0])}}')] hover:bg-[url('{{asset($imgs[1])}}')]--}}
+{{--                bg-cover bg-center bg-no-repeat h-96 w-full"></div>--}}
+        <div class="bg-[url('{{asset($imgs[0])}}')]
+                    bg-cover bg-center bg-no-repeat h-96 w-full"></div>
+    <h5 class="text-2xl text-white">{{$title}}</h5>
+    <p class="text-neon-green">{{$description}}</p>
+    <div class="flex flex-wrap flex-row text-white max-w-80 justify-between">
+        @foreach($languages as $language)
+            <div class="border-2 border-dark-neon-green py-0.5 px-7 w-fit rounded-md text-white mx-1 my-1.5">
+                {{$language}}
+            </div>
+        @endforeach
+    </div>
+    <div class="flex flex-row justify-between">
+        <div>
+            @if($ghLink)
+                <button class="bg-neon-green rounded w-32">
+                    <a class="flex flex-row justify-between py-2 px-1 w-full place-items-center" href="{{$ghLink}}"
+                       target="_blank">
+                        <span class="text-xl">Visit Github</span>
+                        @include('components.icons.github')
+                    </a>
+                </button>
+            @endif
+
+        </div>
+
+        <div>
+            @if($siteLink)
+                <button class="bg-neon-green rounded w-32">
+                    <a class="flex flex-row justify-between py-2 px-1 w-full place-items-center" href="{{$siteLink}}"
+                       target="_blank">
+                        <span class="text-xl">Visit Site</span>
+                        @include('components.icons.link')
+                    </a>
+                </button>
+            @endif
+        </div>
+    </div>
 </section>
--- resources/views/components/portfolio.blade.php
+++ resources/views/components/portfolio.blade.php
@@ -1,8 +1,18 @@
-<section class="border-2 border-neon-green rounded flex flex-col p-1.5 w-full h-[40vh] my-6">
+<section class="border-2 border-neon-green rounded flex flex-col p-3 w-full h-[40vh] my-6">
     <span class="text-neon-green w-full text-2xl text-right">PORTFOLIO</span>
-    <div class="flex flex-row justify-between w-7/12 mx-auto">
-        @include('components.port-card')
-        @include('components.port-card')
-        @include('components.port-card')
+    <div class="flex flex-row justify-between">
+        @include('components.port-card',  [
+                'imgs'=> ['media/escriben.png', 'media/escribenVideo.gif'],
+                'title' => 'Escriben - Markdown Editor',
+                'description' => 'Escriben is a simple markdown editor centered around note taking.
+                Create notebooks to help organize notes. Written with a Laravel back-end and a separate Vue 3 front-end.
+                 This was a lot of fun to build and challenging. ',
+                'languages' => ['Laravel', 'Vue3', 'TypeScript', 'TailwindCss', 'MySql', 'Apache'],
+                'ghLink' => 'https://github.com/digitalvillainy/escriben_fe',
+                'siteLink' => 'https://escriben.io/',
+           ]
+        )
+{{--        @include('components.port-card')--}}
+{{--        @include('components.port-card')--}}
     </div>
 </section>
