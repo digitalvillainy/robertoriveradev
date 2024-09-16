c70e8e99c02682d84c3c8f54e09cd5cad29276ce
rrivera <rrivera@redbannermedia.com>
2024-09-16 04:51:08 -0400

    experience.blade.php set up for 2xl responsiveness

--- resources/views/components/exp-card.blade.php
+++ resources/views/components/exp-card.blade.php
@@ -6,13 +6,14 @@
     'description' => 'Collaborating with other teams of developers to create a full roadmap for development.',
     'msg' => 'You? Contact me today!',
     'time' => 'Currently',
-    'position' => 'left',
+    'lineClass' => 'hidden md:block bg-white h-1.5 w-28 absolute top-1/2 md:-right-28',
+    'boxClass' => 'bg-dark-neon-green size-8 absolute -top-4 md:-right-4',
     'mode'=> 'full'
 ])
 
 @if($mode === 'full')
     <div class="flex flex-row relative">
-        <div class="border-2 border-dark-neon-green rounded flex flex-col p-3 lg:w-[18.2em] xl:w-[30em] text-white justify-between text-xl space-y-3">
+        <div class="border-2 border-dark-neon-green rounded flex flex-col p-3 lg:w-[18.2em] xl:w-[30em] 2xl:w-[20em] text-white justify-between text-xl space-y-3">
             <h3 class="text-3xl">{{ $company }}</h3>
             <div class="flex flex-row justify-between">
                 <span>{{ $title }}</span>
@@ -20,12 +21,18 @@
             </div>
             <p class="font-roboto text-neon-green">{{ $description }}</p>
         </div>
-        <div class="hidden md:block bg-white h-1.5 w-28 absolute top-1/2 md:-right-28">
-            <div class="bg-dark-neon-green size-8 absolute -top-4 md:-right-4"></div>
+        <div @class([
+                "hidden md:block bg-white h-1.5 w-28 absolute top-1/2 md:-right-28",
+                $lineClass
+             ])>
+            <div @class([
+                "bg-dark-neon-green size-8 absolute -top-4 md:-right-4",
+                $boxClass
+            ])></div>
         </div>
     </div>
 @else
-    <div class="border-2 border-dark-neon-green rounded p-1.5 lg:w-[18.2em] xl:w-[30em] text-white text-xl relative mb-4">
+    <div class="border-2 border-dark-neon-green rounded p-1.5 lg:w-[18.2em] xl:w-[30em] 2xl:w-[20em] text-white text-xl relative mb-4">
         <div class="flex flex-row justify-between">
             <span>{{$msg}}</span>
             <span>{{$time}}</span>
--- resources/views/components/experience.blade.php
+++ resources/views/components/experience.blade.php
@@ -1,6 +1,6 @@
-<section class="border-2 border-neon-green rounded flex flex-col p-1.5 h-full xl:h-[72.5vh]">
+<section class="border-2 border-neon-green rounded flex flex-col p-1.5 h-full xl:h-[72.5vh] 2xl:h-[89.5vh]">
     <span class="text-neon-green w-full text-2xl text-right">EXPERIENCE</span>
-    <div class="flex flex-row justify-between md:w-[27.3em] lg:w-[30em] md:mx-auto">
+    <div class="flex flex-row justify-between md:w-[27.3em] lg:w-[30em] 2xl:w-[64em] md:mx-auto">
         <div class="flex flex-col xl:flex-row place-items-start lg:justify-between h-full md:w-80 lg:w-full lg:mx-auto py-8 space-y-6">
             <div class="flex flex-col justify-between h-full lg:h-3/5">
                 <x-exp-card
@@ -29,6 +29,8 @@
                     company="Productive Edge, Chicago Illinois [Remote]"
                     from="April 2021"
                     to="February 2023"
+                    box-class="bg-dark-neon-green size-8 absolute -top-4 md:-right-4 2xl:-left-4"
+                    line-class="hidden md:block bg-white h-1.5 w-28 absolute top-1/2 md:-right-28 2xl:-left-28"
                     description="Planned the front-end architecture for development of websites and web applications for
                     clients Coordinated with back-end developers, project managers, and clients to develop solutions and
                     maintain transparency in project progress. Developed websites and web applications based on project
@@ -41,6 +43,8 @@
                     company="Eagle Productivity Solutions, [Rochester, NY]"
                     from="August 2017"
                     to="January 2019"
+                    box-class="bg-dark-neon-green size-8 absolute -top-4 md:-right-4 2xl:-left-4"
+                    line-class="hidden md:block bg-white h-1.5 w-28 absolute top-1/2 md:-right-28 2xl:-left-28"
                     description="Develop websites and web/mobile applications. Coordinate with a team of other
                     developers and programmers, Project Managers, Graphic Designers, and Production Staff. Work with
                     writers and instructional designers to understand content and offer design, layout, and
