c54e6023e6257fe279a2fe641850cdebf37591de
rrivera <rrivera@redbannermedia.com>
2024-09-16 05:04:29 -0400

    portfolio.blade.php set up for 2xl responsiveness

--- resources/views/components/port-card.blade.php
+++ resources/views/components/port-card.blade.php
@@ -1,9 +1,9 @@
 @props(['thumbnail', 'title', 'description', 'languages', 'ghLink' => false, 'siteLink' => false])
-<section class="border-2 border-neon-green rounded flex flex-col p-1.5 max-w-96 min-h-full max-w-7/12">
+<section class="border-2 border-neon-green rounded flex flex-col p-1.5 max-w-96 min-h-full max-w-7/12 2xl:min-w-10/12">
     <div class="{{$thumbnail}} w-full h-60"></div>
     <div class="my-5 space-y-2 grow">
         <h5 class="text-3xl text-white">{{$title}}</h5>
-        <p class="text-2xl text-dark-neon-green h-full">{{$description}}</p>
+        <p class="font-roboto text-2xl text-dark-neon-green h-full">{{$description}}</p>
     </div>
     <div class="flex flex-wrap flex-row justify-between text-white w-full">
         @foreach($languages as $language)
@@ -12,10 +12,10 @@
             </div>
         @endforeach
     </div>
-    <div class="flex flex-col space-y-3 lg:flex-row justify-between w-full">
+    <div class="flex flex-col space-y-3 lg:flex-row justify-between w-full 2xl:place-items-end">
         <div>
             @if($ghLink)
-                <button class="bg-neon-green rounded w-full lg:w-32 h-10 px-3 hover:bg-white">
+                <button class="bg-neon-green rounded w-full lg:w-32 2xl:w-full h-10 px-3 hover:bg-white">
                     <a class="flex flex-row justify-between w-full place-items-center"
                        href="{{$ghLink}}"
                        target="_blank">
@@ -29,7 +29,7 @@
 
         <div>
             @if($siteLink)
-                <button class="bg-neon-green rounded w-full lg:w-32 h-10 px-3 hover:bg-white">
+                <button class="bg-neon-green rounded w-full lg:w-32 2xl:w-full h-10 px-3 hover:bg-white">
                     <a class="flex flex-row justify-between w-full place-items-center"
                        href="{{$siteLink}}"
                        target="_blank">
--- resources/views/components/portfolio.blade.php
+++ resources/views/components/portfolio.blade.php
@@ -1,34 +1,31 @@
 <section class="border-2 border-neon-green rounded flex flex-col p-4 w-full h-fit mt-5">
     <span class="text-neon-green w-full text-2xl text-right">PORTFOLIO</span>
-    <div class="flex flex-row flex-wrap space-y-5 justify-between content-start w-full md:w-6/12 md:mx-auto">
-        @include('components.port-card',  [
-                'thumbnail'=> 'bg-escriben-img',
-                'title' => 'Escriben - Markdown Editor',
-                'description' => 'Escriben is a simple markdown editor centered around note taking.
-                Create notebooks to help organize notes. Written with a Laravel back-end and a separate Vue 3 front-end.
-                 This was a lot of fun to build and challenging. ',
-                'languages' => ['Laravel', 'Vue3', 'TypeScript', 'TailwindCSS', 'MySql', 'Apache'],
-                'ghLink' => 'https://github.com/digitalvillainy/escriben_fe',
-                'siteLink' => 'https://escriben.io/',
-           ]
-        )
-        @include('components.port-card',  [
-                'thumbnail'=> 'bg-rbm-img',
-                'title' => 'Red Banner Media, LLC',
-                'description' => 'The official home for Red Banner Media, LLC. Created using the TALL stack, allowing for quick implementation. ',
-                'languages' => ['Laravel', 'LiveWire', 'Alpine.js', 'TailwindCSS', 'Mail Chimp', 'Apache'],
-                'ghLink' => 'https://github.com/digitalvillainy/rbmport-2024',
-                'siteLink' => 'https://redbannermedia.com/',
-           ]
-        )
-        @include('components.port-card',  [
-                'thumbnail'=> 'bg-promanage-img',
-                'title' => 'ProMange Vision',
-                'description' => 'While freelancing for Productive Edge, I was a hired off of my abilities with Vue and
-                knowledge of PHP. I was responsible for the architecture of the Front-end of the site and how to integrate Vue 3 and Laravel.',
-                'languages' => ['Laravel', 'Vue3', 'Axios.js', 'TailwindCSS'],
-                'siteLink' => 'https://abc.dev.promanageplan.com/user/login',
-           ]
-        )
+    <div class="flex flex-row flex-wrap space-y-5 justify-between content-start w-full md:w-6/12 md:mx-auto 2xl:w-8/12 2xl:justify-center">
+        <x-port-card
+            thumbnail="bg-escriben-img"
+            title="Escriben - Markdown Editor"
+            description="Escriben is a simple markdown editor centered around note taking.
+            Create notebooks to help organize notes. Written with a Laravel back-end and a separate Vue 3 front-end.
+            This was a lot of fun to build and challenging."
+            :languages="['Laravel', 'Vue3', 'TypeScript', 'TailwindCSS', 'MySql', 'Apache']"
+            ghLink="https://github.com/digitalvillainy/escriben_fe"
+            siteLink="https://escriben.io/"
+        />
+        <x-port-card
+            thumbnail="bg-rbm-img"
+            title="Red Banner Media, LLC"
+            description="The official home for Red Banner Media, LLC. Created using the TALL stack, allowing for quick implementation."
+            :languages="['Laravel', 'LiveWire', 'Alpine.js', 'TailwindCSS', 'Mail Chimp', 'Apache']"
+            ghLink="https://github.com/digitalvillainy/rbmport-2024"
+            siteLink="https://redbannermedia.com/"
+        />
+        <x-port-card
+            thumbnail="bg-promanage-img"
+            title="ProMange Vision"
+            description="While freelancing for Productive Edge, I was a hired off of my abilities with Vue and
+            knowledge of PHP. I was responsible for the architecture of the Front-end of the site and how to integrate Vue 3 and Laravel."
+            :languages="['Laravel', 'Vue3', 'Axios.js', 'TailwindCSS']"
+            siteLink="https://abc.dev.promanageplan.com/user/login"
+        />
     </div>
 </section>
